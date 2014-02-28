14ed69ab2de944fcef3acc630c054a6bd72486aa
Scott Petersen <scott@nutech.com>
2014-02-28 15:29:00 -0800

    Made buttons less annoying.

--- ad2web/setup/forms.py
+++ ad2web/setup/forms.py
@@ -3,9 +3,11 @@
 from flask import Markup
 from flask.ext.wtf import Form
 from flask.ext.wtf.html5 import URLField, EmailField, TelField
+import wtforms
 from wtforms import (Field, ValidationError, HiddenField, TextField, HiddenField,
         PasswordField, SubmitField, TextAreaField, IntegerField, RadioField,
-        FileField, DecimalField, SelectField, BooleanField, SelectMultipleField)
+        FileField, DecimalField, SelectField, BooleanField, SelectMultipleField,
+        FormField)
 from wtforms.validators import (Required, Length, EqualTo, Email, NumberRange,
         URL, AnyOf, Optional)
 from wtforms.widgets import ListWidget, CheckboxInput, html_params
@@ -30,6 +32,10 @@ class BackButtonField(Field):
 
         self.widget = BackButtonWidget(text=label)
 
+class SetupButtonForm(wtforms.Form):
+    previous = BackButtonField(u'Previous')
+    next = SubmitField(u'Next')
+
 class MultiCheckboxField(SelectMultipleField):
     """
     A multiple-select, except displays a list of checkboxes.
@@ -44,23 +50,21 @@ class DeviceTypeForm(Form):
     device_type = SelectField(u'Device Type', choices=[('AD2USB', u'AD2USB'), ('AD2PI', u'AD2PI'), ('AD2SERIAL', u'AD2SERIAL')], default='AD2USB')
     device_location = SelectField(u'Device Location', choices=[('local', 'Local Device'), ('network', 'Network')], default='local')
 
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
 
 class NetworkDeviceForm(Form):
     device_address = TextField(u'Address', [Required(), Length(max=255)], description=u'Hostname or IP address', default='localhost')
     device_port = IntegerField(u'Port', [Required()], description=u'', default=10000)
     ssl = BooleanField(u'Use SSL?')
 
-    previous = BackButtonField(u'Previous')
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
 
 class SSLForm(Form):
     ca_cert = FileField(u'CA Certificate', [Required()], description=u'CA certificate created for the AlarmDecoder to authorize clients.')
     cert = FileField(u'Certificate', [Required()], description=u'Client certificate used by this webapp.')
     key = FileField(u'Key', [Required()], description=u'Client certificate key used by this webapp.')
 
-    previous = BackButtonField(u'Previous')
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
 
 class SSLHostForm(Form):
     config_path = TextField(u'SER2SOCK Configuration Path', default='/etc/ser2sock')
@@ -70,21 +74,17 @@ class SSLHostForm(Form):
     device_port = IntegerField(u'Port', [Required()], description=u'', default=10000)
     ssl = BooleanField(u'Use SSL?')
 
-    previous = BackButtonField(u'Previous')
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
 
 class LocalDeviceForm(Form):
     device_path = TextField(u'Path', [Required(), Length(max=255)], description=u'Filesystem path to your AlarmDecoder.', default='/dev/ttyAMA0')
     baudrate = SelectField(u'Baudrate', choices=[(baudrate, str(baudrate)) for baudrate in BAUDRATES], default=115200, coerce=int)
-
     confirm_management = BooleanField(u'Share AlarmDecoder on your network?', description='This setting serves the AlarmDecoder on your network with ser2sock and allows other software (Software keypad, etc.) to use it in conjunction with this webapp.')
 
-    previous = BackButtonField(u'Previous')
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
 
 class TestDeviceForm(Form):
-    previous = BackButtonField(u'Previous')
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
 
 class DeviceForm(Form):
     keypad_address = TextField(u'Keypad Address', [Required()], default=18)
@@ -94,5 +94,4 @@ class DeviceForm(Form):
     relay_expanders = MultiCheckboxField(u'Relay expanders', choices=[('1', 'Relay #1'), ('2', 'Relay #2'), ('3', 'Relay #3'), ('4', 'Relay #4')])
     deduplicate = BooleanField(u'Deduplicate messages?')
 
-    previous = BackButtonField(u'Previous')
-    next = SubmitField(u'Next')
+    buttons = FormField(SetupButtonForm)
