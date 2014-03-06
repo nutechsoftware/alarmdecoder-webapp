264f362b914d86d3f0b6809fe4847c534e3dd3b9
Kevin Roberts <kevin@nutech.com>
2014-03-06 12:33:32 -0800

    Detect if text selected

--- ad2web/static/js/main.js
+++ ad2web/static/js/main.js
@@ -1,9 +1,24 @@
 /* Author: Wilson Xu */
 
+/* Add a flash message notification to the DOM */
 function add_flash_message(message, category)
 {
     var htmlStr = '<div class="alert alert-' + category + '"><button type="button" class="close" data-dismiss="alert">x</button>' + message + '</div>';
     $('#flash_message_container').append(htmlStr);
+}
+
+/* Determine if text is selected in any input */
+function isTextSelected(input)
+{
+    if( typeof input.selectionStart == "number" )
+    {
+        return input.selectionStart == 0 && input.selectionEnd == input.value.length;
+    }
+    else if( typeof document.selection != "undefined")
+    {
+        input.focus();
+        return document.selection.createRange().text == input.value;
+    }
 }
 
 function hide_flask_message_container() {
