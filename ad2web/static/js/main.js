6fe8b12b61836003874667f7ff811f1d4b54300b
Kevin Roberts <kevin@nutech.com>
2014-03-06 11:39:58 -0800

    Added support for javascript flash messages

--- ad2web/static/js/main.js
+++ ad2web/static/js/main.js
@@ -1,5 +1,11 @@
 /* Author: Wilson Xu */
 
+function add_flash_message(message, category)
+{
+    var htmlStr = '<div class="alert alert-' + category + '"><button type="button" class="close" data-dismiss="alert">x</button>' + message + '</div>';
+    $('#flash_message_container').append(htmlStr);
+}
+
 function hide_flask_message_container() {
     $('#flash_message_container').slideUp('fast');
 }
