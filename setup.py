e31af5b27edbebc2585ff799a39ba68851fbfce3
Scott Petersen <scott@nutech.com>
2014-04-28 12:48:12 -0700

    Added alembic reference.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'sleekxmpp',
         'psutil>=2.0.0',
         'sh',
-        'gitpython',
+        'alembic',
     ],
     dependency_links=[
         'https://github.com/eblot/pyftdi/archive/v0.9.0.tar.gz#egg=pyftdi-0.9.0'
