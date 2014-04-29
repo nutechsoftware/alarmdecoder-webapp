4ad7824fdede796daaa6f20d9ddffafb34fab778
Scott Petersen <scott@nutech.com>
2014-04-29 15:49:22 -0700

    Added support for our database model.

--- alembic/env.py
+++ alembic/env.py
@@ -2,20 +2,19 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+from ad2web.config import DefaultConfig
+from ad2web.extensions import db
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', DefaultConfig.SQLALCHEMY_DATABASE_URI)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
