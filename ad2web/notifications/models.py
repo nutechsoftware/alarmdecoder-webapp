400ef8ea66ef118c25640c0f61319b89fae4fbec
Scott Petersen <scott@nutech.com>
2014-05-13 12:43:07 -0700

    Gutted the old custom notification stuff.

--- ad2web/notifications/models.py
+++ ad2web/notifications/models.py
@@ -6,28 +6,6 @@ from sqlalchemy.orm.collections import attribute_mapped_collection
 
 from ..extensions import db
 
-class CustomNotification(db.Model):
-    __tablename__ = 'custom_notifications'
-
-    id = Column(db.Integer, primary_key=True, autoincrement=True)
-    description = Column(db.String(255), nullable=False)
-    type = Column(db.Integer, nullable=False)
-    user_id = Column(db.Integer, db.ForeignKey('users.id'))
-
-    settings = db.relationship("CustomNotificationSetting", backref="CustomNotification", cascade="all, delete-orphan")
-
-class CustomNotificationSetting(db.Model):
-    __tablename__ = 'custom_notification_settings'
-
-    id = Column(db.Integer, primary_key=True, autoincrement=True)
-    name = Column(db.String(32), nullable=False)
-    custom_notification_id = Column(db.Integer, db.ForeignKey("custom_notifications.id"))
-    message_type = Column(db.Integer, nullable=False)
-    message_address_or_serial = Column(db.Integer)
-    message_channel = Column(db.Integer)
-    message_data = Column(db.Integer)
-    custom_alert_message = Column(db.String(255), nullable=False)
-
 class Notification(db.Model):
     __tablename__ = 'notifications'
 
