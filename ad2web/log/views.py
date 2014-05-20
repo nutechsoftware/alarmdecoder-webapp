f8a805952016733c1ee22408d227047bfe4fdf86
Kevin Roberts <kevin@nutech.com>
2014-05-20 11:14:49 -0700

    Server Side processing of Event Log

--- ad2web/log/views.py
+++ ad2web/log/views.py
@@ -9,9 +9,13 @@ from ..decorators import admin_required
 from .constants import ARM, DISARM, POWER_CHANGED, ALARM, FIRE, BYPASS, BOOT, \
                         CONFIG_RECEIVED, ZONE_FAULT, ZONE_RESTORE, LOW_BATTERY, \
                         PANIC, RELAY_CHANGED, EVENT_TYPES
+from sqlalchemy import func
 from .models import EventLogEntry
+import json
+import collections
 
 log = Blueprint('log', __name__, url_prefix='/log')
+
 
 @log.context_processor
 def log_context_processor():
@@ -35,9 +39,9 @@ def log_context_processor():
 @log.route('/')
 @login_required
 def events():
-    event_log = EventLogEntry.query.order_by(EventLogEntry.timestamp.desc()).all()
+#    event_log = None #EventLogEntry.query.order_by(EventLogEntry.timestamp.desc()).limit(50)
 
-    return render_template('log/events.html', event_log=event_log, active='events')
+    return render_template('log/events.html')
 
 @log.route('/live')
 @login_required
@@ -52,3 +56,88 @@ def delete():
     events = EventLogEntry.query.delete()
     db.session.commit()
     return redirect(url_for('log.events'))
+
+#XHR for retrieving event log data server side
+@log.route('/retrieve_events_paging_data')
+@login_required
+def get_events_paging_data():
+
+    #get results from datatable via XHR
+    results = DataTablesServer(request).output_result()
+
+    return json.dumps(results)
+
+class DataTablesServer:
+    def __init__( self, request ):
+        #values specified by datatable for filtering, sorting, paging etc
+        self.request_values = request.values
+        self.result_data = None
+
+        #total in table unfiltered
+        self.cardinality = 0
+        #total in table after filter
+        self.cardinality_filtered = 0
+        
+        self.run_queries()
+
+    def output_result(self):
+        output = {}
+        output['sEcho'] = str(int(self.request_values['sEcho']))
+        output['iTotalRecords'] = int(self.cardinality);
+        output['iTotalDisplayRecords'] = int(self.cardinality);
+
+        aaData_rows = []
+
+        #iterate the result and append data rows
+        for row in self.result_data:
+            aaData_row = []
+            aaData_row.append(str(row.timestamp))
+            aaData_row.append(EVENT_TYPES[row.type])
+            aaData_row.append(row.message)
+
+            aaData_rows.append(aaData_row)
+
+        output['aaData'] = aaData_rows
+        return output
+
+    def run_queries(self):
+        pages = self.paging()
+        filter = self.filtering()
+
+        #page to start on
+        start = 0
+        #number of records to return
+        limit = 10
+
+        #non-default values chosen from the UI
+        if pages.start is not None:
+            start = pages.start
+        if pages.length is not None:
+            limit = pages.length
+
+        #if filtered, cardinality based off filter, otherwise all rows
+        if filter is not None:
+            self.result_data = EventLogEntry.query.filter(EventLogEntry.message.like('%' + filter + '%')).order_by(EventLogEntry.timestamp.desc()).limit(limit).offset(start)
+            self.cardinality_filtered = self.result_data.count()
+            self.cardinality = EventLogEntry.query.filter(EventLogEntry.message.like('%' + filter + '%')).count()
+        else:
+            self.result_data = EventLogEntry.query.order_by(EventLogEntry.timestamp.desc()).limit(limit).offset(start)
+            self.cardinality_filtered = self.result_data.count()
+            self.cardinality = EventLogEntry.query.order_by(EventLogEntry.timestamp.desc()).count()
+
+    #here we determine the filter value for the search box and apply to the queries
+    def filtering(self):
+        filter = None
+        if( self.request_values.has_key('sSearch')) and (self.request_values['sSearch'] != "" ):
+            filter = self.request_values['sSearch']
+
+        return filter
+
+    #determine what page we're on, as well as how many to show per page
+    def paging(self):
+        pages = collections.namedtuple('pages', ['start', 'length'])
+        if( self.request_values['iDisplayStart'] != "" ) and (self.request_values['iDisplayLength'] != -1 ):
+            pages.start = int(self.request_values['iDisplayStart'])
+            pages.length = int(self.request_values['iDisplayLength'])
+
+        return pages;
