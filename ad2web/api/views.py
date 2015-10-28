7ad25cf242fec46e23f38dc0850692553ee80359
Scott Petersen <scott@nutech.com>
2015-10-28 14:08:59 -0700

    API starting point.

--- ad2web/api/views.py
+++ ad2web/api/views.py
@@ -6,9 +6,9 @@ from flask.ext.login import login_user, current_user, logout_user
 from ..user import User
 
 
-api = Blueprint('api', __name__, url_prefix='/api')
+api = Blueprint('api', __name__, url_prefix='/api/v1')
 
-
+# TODO: KEEP OR REMOVE THESE?
 @api.route('/login', methods=['POST'])
 def login():
     if current_user.is_authenticated():
@@ -31,3 +31,80 @@ def logout():
     if current_user.is_authenticated():
         logout_user()
     return jsonify(flag='success', msg='Logouted.')
+
+# /TODO
+
+##### AlarmDecoder device routes
+@api.route('/alarmdecoder', methods=['GET'])
+def alarmdecoder():
+    return jsonify(status='OK')
+
+@api.route('/alarmdecoder/send', methods=['POST'])
+def alarmdecoder_send():
+    pass
+
+@api.route('/alarmdecoder/reboot', methods=['POST'])
+def alarmdecoder_reboot():
+    pass
+
+@api.route('/alarmdecoder/configuration', methods=['GET', 'PUT'])
+def alarmdecoder_configuration():
+    pass
+
+
+##### Zone routes
+@api.route('/zones', methods=['GET', 'POST'])
+def zones():
+    pass
+
+@api.route('/zones/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def zones_by_id(id):
+    pass
+
+@api.route('/zones/<int:id>/fault', methods=['POST'])
+def zones_fault(id):
+    pass
+
+
+##### Notification routes
+@api.route('/notifications', methods=['GET', 'POST'])
+def notifications():
+    pass
+
+@api.route('/notifications/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def notifications_by_id(id):
+    pass
+
+
+##### Camera routes
+@api.route('/cameras', methods=['GET', 'POST'])
+def cameras():
+    pass
+
+@api.route('/cameras/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def cameras_by_id(id):
+    pass
+
+
+##### User routes
+@api.route('/users', methods=['GET', 'POST'])
+def users():
+    pass
+
+@api.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def users_by_id(id):
+    pass
+
+
+##### System routes
+@api.route('/system', methods=['GET'])
+def system():
+    pass
+
+@api.route('/system/reboot', methods=['POST'])
+def system_reboot():
+    pass
+
+@api.route('/system/shutdown', methods=['POST'])
+def system_shutdown():
+    pass
