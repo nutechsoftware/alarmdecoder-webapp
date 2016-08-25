d6668a67b0f61c1458c162464ea1200079890455
Kevin Roberts <kevin@nutech.com>
2016-08-25 14:58:11 -0700

    Updating blank targets to honor noopener noreferrer

--- ad2web/static/swagger/swagger-ui.js
+++ ad2web/static/swagger/swagger-ui.js
@@ -349,7 +349,7 @@ this["Handlebars"]["templates"]["main"] = Handlebars.template({"1":function(dept
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.externalDocs : depth0)) != null ? stack1.description : stack1), depth0))
     + "</p>\n  <a href=\""
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.externalDocs : depth0)) != null ? stack1.url : stack1), depth0))
-    + "\" target=\"_blank\">"
+    + "\" target=\"_blank\" rel=\"noopener noreferrer\">"
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.externalDocs : depth0)) != null ? stack1.url : stack1), depth0))
     + "</a>\n";
 },"4":function(depth0,helpers,partials,data) {
