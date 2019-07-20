4dd0739d0fa0e199ceb32e923ced477f5b092466
f34rdotcom <coder@f34r.com>
2019-07-19 20:10:13 -0700

    Another backwards compatability fix.

--- ad2web/extensions.py
+++ ad2web/extensions.py
@@ -6,8 +6,15 @@ db = SQLAlchemy()
 from flask_mail import Mail
 mail = Mail()
 
-from flask_cache import Cache
-cache = Cache()
+# Load older cache extension if new caching  module is not available.
+# Flash Cache is EOL and is being replaced with caching
+try:
+    ## New
+    from flask_caching import Cache
+    cache = Cache()
+except ImportError:
+    ## Old remove 2022?
+    from .extensions import cache
 
 from flask_login import LoginManager
 login_manager = LoginManager()
